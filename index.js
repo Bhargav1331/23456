08fd16058f7a4bda114d3053dad0e04c0a3fbd2c
Bhargav1331 <123548291+Bhargav1331@users.noreply.github.com>
2023-04-28 17:08:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require('express');
+
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-const port = process.env.PORT || 3000
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+const PORT = process.env.PORT || 3000;
+const data = {
+  name: 'John Doe',
+  age: 30,
+  email: 'johndoe@example.com'
+};
+
+app.get('/api/data', (req, res) => {
+  res.json(data);
 });
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
-});
-server.listen(port, () => {
-    console.log('listening on *:3000');
+app.listen(PORT, () => {
+  console.log('Server started on port 3000');
 });
